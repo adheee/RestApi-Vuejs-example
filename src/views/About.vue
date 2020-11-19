55f8a6540bfe081679e8de71a6847f82e8e8fee2
adheee <adheejr77@gmail.com>
2020-11-19 17:27:42 +0800

    RestApi Vue.js

---
+++ src/views/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about">
+    <h1>Test API</h1>
+
+    <ul>
+      <li v-for="provinsis in data" :key="provinsis">
+        <span>{{ provinsis.nama_provinsi }}</span> &#160;
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+ul {
+  li {
+    list-style: none;
+    margin-bottom: 10px;
+  }
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      data: [],
+    };
+  },
+  created() {
+    this.getAllUser();
+  },
+  methods: {
+    getAllUser() {
+      axios
+        .get("https://galung-app.asdar12.my.id/api/provinsi")
+        .then((response) => {
+          this.data = response.data.data;
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
